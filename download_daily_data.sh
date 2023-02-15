2fffd776ae2da2104affd2e9e25c85e4d669109f
Shivam <shivam.kaushik@1mg.com>
2023-02-15 09:17:24 +0530

    Added new data

---
+++ download_daily_data.sh
@@ -0,0 +1,10 @@
+python livestreamtest.py \
+    --scrip=NSE:NIFTY \
+    --num_candles=300
+
+python livestreamtest.py \
+    --scrip=NSE:BANKNIFTY \
+    --num_candles=300
+
+cd NSE:NIFTY && dvc add *.csv && cd ..
+cd NSE:BANKNIFTY && dvc add *.csv && cd ..
